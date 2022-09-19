7c984e0641eb32a0d8d326b0450e51d2bc318bc3
Trinh Duy An <luongduongduc4@gmail.com>
2022-09-19 16:42:55 +0700

    Test

---
+++ some-react/listStudent/js/api.js
@@ -0,0 +1,59 @@
+const API_ROOT = 'http://localhost:3000/users'
+
+function getUsersAPI(renderFunction) {
+    $.ajax({
+        method: 'GET',
+        url: API_ROOT,
+    }).done(function (users) {
+        if (renderFunction) renderFunction(users)
+    })
+}
+
+function getUserByIdAPI(id, renderFunction) {
+    $.ajax({
+        method: 'GET',
+        url: API_ROOT + '/' + id,
+    }).done(function (user) {
+        renderFunction(user)
+    }).catch(function () {
+        renderFunction(null)
+    })
+}
+
+function createUserAPI(user, redirectLink) {
+    $.ajax({
+        method: 'POST',
+        url: API_ROOT,
+        data: user
+    }).done(function () {
+        if (redirectLink) window.location.href = redirectLink
+    })
+}
+
+function updateUserAPI(userId, userInfo, redirectLink) {
+    $.ajax({
+        method: 'PUT',
+        url: API_ROOT + '/' + userId,
+        data: userInfo
+    }).done(function () {
+        if (redirectLink) window.location.href = redirectLink
+    })
+}
+
+function deleteUserAPI(id, renderFunction) {
+    $.ajax({
+        method: 'DELETE',
+        url: API_ROOT + '/' + id
+    }).done(function () {
+        if (renderFunction) renderFunction()
+    })
+}
+
+function getFormAPI(renderFunction) {
+    $.ajax({
+        method: 'GET',
+        url: '/form.html',
+    }).done(function (html) {
+        if (renderFunction) renderFunction(html)
+    })
+}
---
+++ some-react/listStudent/js/fix.js
@@ -0,0 +1,35 @@
+
+        $(function () {
+            getUsersAPI(renderContent);
+        })
+
+        function renderContent(users) {
+            let htmlContent = '';
+
+            for (let i = 0; i < users.length; i++) {
+                htmlContent += '<tr>';
+                htmlContent += '<td>' + users[i].name + '</td>';
+                htmlContent += '<td>' + (users[i].birthYear || 'Chưa biết') + '</td>';
+                htmlContent += '<td>' + users[i].email + '</td>';
+                htmlContent += '<td>' + users[i].phone + '</td>';
+                htmlContent += '<td>' +
+                    '<a href="edit.html?id=' + users[i].id + '" class="text-info"><i class="fa fa-edit"></i> Chỉnh sửa</a> | ' +
+                    '<a href="javascript:void(0)" class="text-danger" onclick="deleteUser(' + users[i].id + ', this)"><i class="fa fa-trash-alt"></i> Xóa</a>' +
+                    '</td>';
+                htmlContent += '</tr>';
+            }
+
+            $('table tbody').html(htmlContent);
+        }
+
+        function deleteUser(id, aElement) {
+            const result = confirm('Bạn có chắc chắn muốn xóa học viên này?');
+            if (result) {
+                deleteUserAPI(id, function () {
+                    const tdElement = $(aElement).parent();
+                    const trElement = tdElement.parent();
+                    trElement.remove();
+                })
+            }
+        }
+    
